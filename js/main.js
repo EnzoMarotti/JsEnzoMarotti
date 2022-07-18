3bd8b14c77afac1d509c8302f75e77aba45e3f02
enzo_marotti <enzomarotti15@gmail.com>
2022-07-18 00:19:30 -0300

    probando ciclos

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// const AñoActual = 2022;
+
+// AñoVehiculo = parseInt (prompt ("Ingrese el año de su vehículo"));
+
+// let antiguedad = AñoActual- AñoVehiculo;
+
+// if ( antiguedad <= 10) {
+//     alert ("Su vehiculo debe abonar 200 dolares de patente");
+// }
+
+// else if (antiguedad > 10) {
+//     alert ("Su vehiculo no debe abonar patente")
+// }
+
+// else {
+//     alert ("ingrese un valor numerico valido")
+// }
+
+
+
+// Ciclos Por conteo
+
+let i= 1;
+
+// for (let i=1; i<= 10; i++) {
+//     alert (i);
+// }
+
+
+// Multiplicar
+
+let ingresarNumero = parseInt(prompt ("ingrese un numero"));
+
+for (let i=1; i<=10; i++) {
+    let resultado = ingresarNumero * i;
+
+
+console.log (ingresarNumero + " X "+ i + "=" + resultado);
+}
