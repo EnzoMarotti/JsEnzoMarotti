43743e635550a3a7e72e958f69f6a5594b386897
enzo_marotti <enzomarotti15@gmail.com>
2022-08-11 00:26:16 -0300

    Eventos casi completo. Trabajando en el carrito

--- js/main.js
+++ js/main.js
@@ -1,91 +1,56 @@
-class Serv{
-    constructor(Servicio, precio){
-        this.Servicio = Servicio.toLowerCase();
-        this.precio = (precio);
+const carrito=[];
+
+class Producto {
+    constructor(nombre, imagen, precio) {
+        this.nombre = nombre.toUpperCase();
+        this.imagen = imagen;
+        this.precio = parseInt(precio);
+      }
+}
+
+const PS4 = []
+
+
+PS4.push(new Producto("Elden Ring","../img/eldenRing.jpg", 12000));
+PS4.push(new Producto("Horizon Forbidden West", "../img/horizon.jpg", 12000));
+PS4.push(new Producto("Resident Evil 8 Village", "../img/RE8.png", 9000));
+
+
+
+const Gamecards=document.getElementById ("PS4");
+
+const mostrar = () =>{
+    for (let juegos of PS4) {
+        Gamecards.innerHTML +=
+        `
+        <div class="card" style="width: 18rem;">
+            <img src="${juegos.imagen}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${juegos.nombre}</h5>
+                <p class="card-text">${juegos.precio}</p>
+                <a href="#" id="btnCompra" class="btn btn-primary">agregar al carrito</a>
+            </div>
+        </div>
+        `
     }
 }
 
-const Servicios=[]
+
+
+const btnPS4 = document.getElementById ("btnPS4");
+
+btnPS4.addEventListener('click', mostrar);
 
 
 
-let ingreso= prompt  ("Ingrese el nombre de la comida que desea ordenar: \nrissoto de calabaza. \nwrap proteico. \nchicken pasta. \nprotein pizza. \nprotein pancakes. \nprotein smooties. \n Para terminar la orden, escriba 'f' y dele a aceptar.").toLowerCase();
-
-let precio=0;
-
-function calcular(){
-
- while (ingreso != "f") {
-    
-    
-    
-    switch(ingreso) {
-    case "rissoto de calabaza":
-    Servicios.push  (new Serv ("rissoto de calabaza",1100));
-    precio = precio + 1100;
-    break;
-    
-    case "wrap proteico":
-    Servicios.push  (new Serv ("wrap proteico",500));
-    precio= precio + 500;
-    break;
-    
-    case "chicken pasta":
-    Servicios.push  (new Serv ("chicken pasta",900));
-    precio = precio + 900;
-    break;
-    
-    case "protein pizza":
-    Servicios.push  (new Serv ("protein pizza",899));
-    precio= precio + 899;
-    break;
-    
-    case "protein pancakes":
-    Servicios.push  (new Serv ("protein pancakes",400));
-    precio = precio + 400;
-    break;
-    
-    case "protein smooties":
-    Servicios.push  (new Serv ("protein smooties",500));
-    precio = precio + 500;
-    break;
-    
+const btnAgregarCarrito = document.getElementById ("btnCompra");
 
 
-
+function AgregarItem (productoId) {
+    carrito.push (productoId)
 }
 
-
-
-
-if (precio == 0){
-    console.log("usted no ha selecciono nada")
+btnAgregarCarrito.onclick = function (AgregarItem) {
+    AgregarItem ();
 }
-
-console.table(Servicios);
-
-ingreso = prompt  ("Ingrese el nombre de la comida que desea ordenar: \nrissoto de calabaza, \nwrap proteico, \nchicken pasta, \nprotein pizza, \nprotein pancakes, \nprotein smooties. \n Para terminar la orden, escriba 'f' y dele a aceptar.").toLowerCase();
-      
-}
-
-
-
-let filt = Servicios.filter ((ser) => ser.precio < 500)
-console.log(filt);
-let filt2 = Servicios.filter ((ser) => ser.precio >= 500 && ser.precio < 900)
-console.log(filt2);
-let filt3 = Servicios.filter ((ser) => ser.precio >= 900 && ser.precio < 1200)
-console.log(filt3);
-let filt4 = Servicios.filter ((ser) => ser.precio >= 1200 && ser.precio < 1500)
-console.log(filt4);
-
-ivaIncluido=precio*1.21;
-
-console.log("precio final"+" $"+ ivaIncluido + (" (iva incluido)"));
-
-
-
-
-}
-
-calcular();
+//Todavia no sale el carrito//
